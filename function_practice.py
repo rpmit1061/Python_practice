ed5d695d2dd7e137f08aae3faef9870deccc7102
Rahul Patel <rpmit1061@gmail.com>
2020-05-18 15:55:04 +0530

    practice pythone

--- function_practice.py
+++ function_practice.py
@@ -1,10 +1,2 @@
-for test in range(int(input())):
-    try:
-        a,b = map(int,input().split())
-        division_result = a // b
-        print(division_result)
-    except ZeroDivisionError as e:
-        print("Error Code:", e)
-    except ValueError as e:
-        print("Error Code:", e)
-
+name = input('what is your name? ')
+print('Hi '+ name)
