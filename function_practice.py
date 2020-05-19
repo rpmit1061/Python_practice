759da50205a832d9405395aa75b8cbf85a38db23
Rahul Patel <rpmit1061@gmail.com>
2020-05-19 17:55:27 +0530

    practice pythone

--- function_practice.py
+++ function_practice.py
@@ -5,4 +5,4 @@ print(name + ' likes ' + fev_color)
 #Program for calculate age :
 birth_year = input('Enter Your birth Year ')
 age = 2020 - int(birth_year)
-print('You are ' ,, age , ' old')
+print('You are ' , age , ' old')
